b2551bbfb976c7c12eb056584ca8d7f735b24b5f
nakamura <nakamura@>
2017-12-27 00:52:36 +0900

    2017-12-27-00-52

--- door.rb
+++ door.rb
@@ -4,104 +4,132 @@ require File.dirname(__FILE__) + '/log'
 
 
 db = SQLite3::Database.new 'test.sqlite3'
-key_number = 0
-num = 1
-nfc_port = "/dev/ttyUSB" + num.to_s
-key_port = "/dev/ttyUSB" + key_number.to_s
 baud = 9600
-spw = SerialPort.new(key_port, baud)
-file_name = "status.txt"
-status = ""
+posix = "/dev/ttyUSB"
+windows = "COM"
+serial_port = posix
 
-puts "KEYPORT : " + key_port
+for key_number in 0..9
+	key_name = serial_port + key_number.to_s
+
+	begin
+		key_sp = SerialPort.new(key_name, baud)
+		sleep(0.5)
+		who = key_sp.readline
+
+		if who == ""
+			raise
+		elsif who != "KEY"
+			if key_number == 9
+				puts "Can't find KEYPORT."
+				exit
+			else
+				next
+			end
+		else
+			key_sp.read_timeout = 100
+			break
+		end
+	rescue
+		sleep(1)
+		retry
+	end
+end
+
+for nfc_number in 0..9
+	if nfc_number == key_number
+		next
+	end
+
+	nfc_name = serial_port + nfc_number.to_s
+
+	begin
+		nfc_sp = SerialPort.new(nfc_name, baud)
+		sleep(0.5)
+		who = nfc_sp.readline
+
+		if who == ""
+			raise
+		elsif who != "NFC"
+			if nfc_number == 9
+				puts "Can't find NFCPORT."
+				exit
+			else
+				next
+			end
+		else
+			nfc_sp.read_timeout = 100
+			break
+		end
+	rescue
+		sleep(1)
+		retry
+	end
+end
+
+puts "KEYPORT : " + key_name
+puts "NFCPORT : " + nfc_name
 
 loop do
 	begin
-		spr = SerialPort.new(nfc_port, baud)
-		spr.read_timeout = 100
-		
-		puts "NFCPORT : " + nfc_port
-
+		tmp = ""
 		loop do
 			begin
-				tmp = ""
-				loop do
-					begin
-						tmp += spr.readline
-						if tmp.index("\n")
-							break
-						end
-					rescue EOFError
-						retry
-					end
+				tmp += nfc_sp.readline
+				if tmp.index("\n")
+					break
 				end
-				tmp = tmp.split(/\s*\n\s*/)
-				max = 0
-				nfc = ""
-
-				tmp.each { |chr|
-					if chr.size > max
-						max = chr.size
-						nfc = chr
-					end
-				}
-
-				#puts "NFC : " + nfc
-
-				if nfc != ""
-					
-					str = 'select * from users where nfc=\'' + nfc + '\''
-					user = db.execute(str)
-
-					if user.length > 0
-
-						begin
-							status = File.open(file_name).read.chomp
-						rescue
-							file = File.open(file_name, "w")
-                            file.puts("OPEN ")
-                            file.close
-							retry
-						end
-
-						if status == "OPEN "
-							str = '0'
-							status = "CLOSE"
-						else
-							str = '1'
-							status = "OPEN "
-						end
-
-						begin
-							spw.write str
-							file = File.open(file_name, "w")
-							file.puts(status)
-							file.close
-						rescue
-							puts "Error, cannot write key_port."
-						end
-
-						puts log(status, user[0][1])
-						puts ""
-					end
-
-				end
-
-			rescue => e
-				puts e.message
-				sleep(1)
+			rescue EOFError
 				retry
 			end
 		end
-	rescue
-		if num >= 9
-			num = 1
-		elsif num == key_number
-			num = num + 2
-		else
-			num = num + 1
+		tmp = tmp.split(/\s*\n\s*/)
+		max = 0
+		nfc = ""
+
+		tmp.each { |chr|
+			if chr.size > max
+				max = chr.size
+				nfc = chr
+			end
+		}
+
+		if nfc != ""
+			
+			str = 'select * from users where nfc=\'' + nfc + '\''
+			user = db.execute(str)
+
+			if user.length > 0
+
+				begin
+					key_sp.write "0"
+					sleep(0.5)
+					status = key_sp.readline
+					if status == ""
+						raise
+					end
+				rescue => e
+					puts e
+					sleep(1)
+					retry
+				end
+
+				begin
+					key_sp.write "1"
+				rescue
+					puts "Error, cannot write " + key_name + "."
+				end
+
+				puts log(status, user[0][1])
+				puts ""
+
+			end
+
 		end
 
-		nfc_port = '/dev/ttyUSB' + num.to_s
+	rescue => e
+		puts e.message
+		sleep(1)
+		retry
 	end
 end
