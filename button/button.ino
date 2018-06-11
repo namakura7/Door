3b1cf5fa9d6272d5c891ba0d3c4b9cf06cab63a3
nakamura <nakamura@macbook>
2018-06-11 23:16:02 +0900

    change button.ino

--- button/button.ino
+++ button/button.ino
@@ -1,3 +1,7 @@
+#define SWITCH 2
+#define LED 7
+#define WAIT_TIME 2000
+
 void setup() {
   Serial.begin(9600);
 }
@@ -7,20 +11,15 @@ void loop() {
     char in = Serial.read();
 
     if(in == 'w'){
-      Serial.println("NFC");
+      Serial.println("BUTTON");
     }
   }
 
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-  String strBuf[mfrc522.uid.size];
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-      strBuf[i] =  String(mfrc522.uid.uidByte[i], HEX);  // (E)using a constant integer
-      if(strBuf[i].length() == 1){  // 1桁の場合は先頭に0を追加
-        strBuf[i] = "0" + strBuf[i];
-      }
-      Serial.print(strBuf[i].c_str());
+  if (digitalRead(SWITCH) == LOW)
+  {
+    digitalWrite(LED, HIGH);
+    Serial.println("PUSH");
+    delay(WAIT_TIME);
+    digitalWrite(LED, LOW);
   }
-  Serial.println();
-  delay(1500);
 }
