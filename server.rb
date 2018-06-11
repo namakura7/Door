741d78b07a8e1e95025025c9e8a7a203e3a50a75
nakamura <nakamura@macbook>
2018-06-11 23:00:33 +0900

    chabge server.rb

--- server.rb
+++ server.rb
@@ -7,43 +7,45 @@ def serial_port_name num
 	return '/dev/ttyUSB' + num.to_s
 end
 
-
-# 周波数
-baud = 9600
-file_name = "status.txt"
-
-begin
-	status = File.open(file_name).read.chomp
-rescue
-	File.open(file_name, "w").puts("OPEN ")
-	retry
-end
-
-if status == "OPEN "
-	str = '0'
-	status = "CLOSE"
-else
-	str = '1'
-	status = "OPEN "
-end
-
-(0..9).each { |num|
-	key_port = serial_port_name(num)
+def server
+	# 周波数
+	baud = 9600
+	file_name = "status.txt"
 
 	begin
-		sp = SerialPort.new(key_port, baud)
-		# 1.7秒のsleepが必要
-		sleep(1.7)
-		sp.write str
-		sp.close
-		break
+		status = File.open(file_name).read.chomp
 	rescue
-		next
+		File.open(file_name, "w").puts("OPEN ")
+		retry
 	end
-}
 
-file = File.open(file_name, "w")
-file.puts(status)
-file.close
+	if status == "OPEN "
+		str = '0'
+		status = "CLOSE"
+	else
+		str = '1'
+		status = "OPEN "
+	end
 
-log(status, "SERVER")
+	(0..9).each { |num|
+		key_port = serial_port_name(num)
+
+		begin
+			sp = SerialPort.new(key_port, baud)
+			# 1.7秒のsleepが必要
+			sleep(1.7)
+			sp.write str
+			sp.close
+			break
+		rescue
+			next
+		end
+	}
+
+	file = File.open(file_name, "w")
+	file.puts(status)
+	file.close
+
+	log(status, "SERVER")
+end
+
