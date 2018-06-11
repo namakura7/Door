f16a97df4b0b428ae5cd057093bbcefdebafb9d9
nakamura <nakamura@macbook>
2018-06-11 22:21:37 +0900

    change log.rb

--- log.rb
+++ log.rb
@@ -1,8 +1,8 @@
-require 'date'
+require "date"
 
 def change(num)
 	if num < 10
-		return "0" << num.to_s
+		return "0#{num.to_s}"
 	else
 		return num.to_s
 	end
@@ -12,7 +12,25 @@ def log(status, username)
 	date = Date.today
 	time = DateTime.now
 
-	str = date.strftime("%Y/%m/%d, ") << change(time.hour) << ":" << change(time.min) << ":" << change(time.sec) << " ## " << status << " ## " << username
+	wday =
+		case date.wday
+			when 0 then
+				"Sun"
+			when 1 then
+				"Mon"
+			when 2 then
+				"Tue"
+			when 3 then
+				"Wed"
+			when 4 then
+				"Thu"
+			when 5 then
+				"Fri"
+			else
+				"Sat"
+		end
+
+	str = "#{date.strftime("%Y/%m/%d")}, #{wday}, #{change(time.hour)}:#{change(time.min)}:#{change(time.sec)} ## #{status} ## #{username}"
 
 	File.open("log.txt", "a").puts(str)
 
